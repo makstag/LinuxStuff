b6b2bd3be3e36731af96a1d34a17fa495ee586d3
Maxim Titov <mstitovforgit@gmail.com>
2024-01-25 17:59:46 +0300

    update sconf

--- include/sconf.h
+++ include/sconf.h
@@ -1,3 +1,6 @@
+#define _GNU_SOURCE
+#define _POSIX_C_SOURCE 199309
+
 #include <sys/un.h>
 #include <sys/socket.h>
 #include <stdio.h>
@@ -7,15 +10,11 @@
 #include <iostream>
 #include <chrono>
 #include <limits.h>
-
+#include <signal.h>
 
 #define BUF_SIZE 210000
 #define SOCK_PATH "/tmp/saddr"
 #define LEN 1024
-
-#define _GNU_SOURCE
-#define _POSIX_C_SOURCE 199309
-#include <signal.h>
 
 using Clock = std::chrono::steady_clock;
 using namespace std::chrono;
