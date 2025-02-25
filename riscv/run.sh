dcfbd9b0f1e84daa2528c3de56bfacb0521bb6ca
makstag <mstitovforgit@gmail.com>
2025-02-25 21:58:11 +0300

    added first step implementation riscvOS

---
+++ riscv/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -xue
+
+# Путь QEMU
+QEMU=qemu-system-riscv32
+
+# Путь к clang и его флагам
+CC=clang  # Для Ubuntu: используйте CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# Сборка ядра
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c common.c
+
+# Запуск QEMU
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+	-kernel kernel.elf
