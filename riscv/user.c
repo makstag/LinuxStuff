4803c0a4698159bde92b6a72624e5d63050dd4f1
makstag <mstitovforgit@gmail.com>
2025-03-08 17:08:43 +0300

    add first user application

---
+++ riscv/user.c
@@ -0,0 +1,16 @@
+#include "user.h"
+
+extern char __stack_top[];
+
+__attribute__((noreturn)) void exit(void)
+{
+    for (;;)
+        ;
+}
+
+__attribute__((section(".text.start"))) __attribute__((naked)) void start(void)
+{
+    __asm__ __volatile__("mv sp, %[stack_top] \n"
+                         "call main           \n"
+                         "call exit           \n" ::[stack_top] "r"(__stack_top));
+}
